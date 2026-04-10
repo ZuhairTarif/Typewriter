203c5a161ad512c632d0bd6302c847e8b01cc42f
Zuhair Ahmed <tarif4667@gmail.com>
2026-04-10 10:37:47 +0600

    Initial Commit

---
+++ Typewriter/TypewriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypewriterApp.swift
+//  Typewriter
+//
+//  Created by mac on 10/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TypewriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
